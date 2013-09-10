158013179869fe00fc3706550e5a97c2d58c1029
Alexey Alekhin <laughedelic@gmail.com>
2013-09-10 19:45:54 +0200

    Updated to statika-0.15-snapshot with new install methods

--- build.sbt
+++ build.sbt
@@ -4,8 +4,15 @@ description := "A bundle for gcc tool"
 
 homepage := Some(url("https://github.com/ohnosequences/gcc"))
 
+organization := "ohnosequences"
+
+organizationHomepage := Some(url("http://ohnosequences.com"))
+
 licenses := Seq("AGPLv3" -> url("http://www.gnu.org/licenses/agpl-3.0.txt"))
 
 bundleObjects := Seq("ohnosequences.statika.GCC")
 
- 
+
+statikaVersion := "0.15.0-SNAPSHOT"
+
+awsStatikaVersion := "0.2.0-SNAPSHOT"
--- src/main/scala/Gcc.scala
+++ src/main/scala/Gcc.scala
@@ -7,10 +7,7 @@ case object GCC extends Bundle() {
 
   val metadata = generated.metadata.GCC
 
-  def install[D <: DistributionAux](distribution: D): InstallResults = {
-    val result = Seq("yum", "install", "gcc", "-y").!
-    if (result == 0) success(metadata+" is installed")
-    else failure("Couldn't install "+metadata)
-  }
+  def install[D <: AnyDistribution](distribution: D): InstallResults =
+    Seq("yum", "install", "gcc", "-y") ->- success(metadata+" is installed")
 
 }
